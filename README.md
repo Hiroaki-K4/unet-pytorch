b43817aa7729a436190fecdca7ee678d17c4ae9b
Hiroaki-K4 <49no19@gmail.com>
2025-01-01 19:45:58 -0800

    Add double conv

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ conda activate unet
 conda install pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia
 pip install -r requirements.txt
 ```
+
+```
+The contracting path follows the typical architecture of a convolutional network.
+It consists of the repeated application of two 3x3 convolutions (unpadded convolutions),
+each followed by a rectiﬁed linear unit (ReLU) and a 2x2 max pooling operation with stride 2
+for downsampling.
+```
---
+++ srcs/unet.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels=1, out_channels=64):
+        super(DoubleConv, self).__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, input_dim=2, hidden_dim=64):
+        super(UNet, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim + 1, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, input_dim),
+        )
+
+    def forward(self, x, t):
+        input_data = torch.hstack([x, t])
+        return self.net(input_data)
+
+
+if __name__ == "__main__":
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    print("Using {0} device".format(device))
+    double_conv = DoubleConv().to(device)
+    print("Double conv: ", double_conv)
+    print()
+    model = UNet().to(device)
+    print(model)
