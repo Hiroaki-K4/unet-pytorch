b1580442baa22ebbf1ecdeae755a9c5405a2cbbc
Hiroaki-K4 <49no19@gmail.com>
2025-01-16 12:20:38 -0800

    Update training code

--- srcs/train.py
+++ srcs/train.py
@@ -17,11 +17,15 @@ def train(aug_data_path, device):
     anno_paths = [path for path in img_path_list if path.endswith("_anno.png")]
     img_paths.sort()
     anno_paths.sort()
-    print(img_paths)
-    print(anno_paths)
-    # TODO: Create label data
+    model = UNet().to(device)
+    for i in range(len(img_paths)):
+        img_path = img_paths[i]
+        img = cv2.imread(img_path)
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # TODO: Fix error
+        model.forward(gray_img)
+        input()
 
-    # model = UNet().to(device)
     # print(model)
 
 
