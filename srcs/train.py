570ca0a5eeb7ebe22d4adaa4b2a59fc2c9c8c8fa
Hiroaki-K4 <49no19@gmail.com>
2025-01-18 17:07:51 -0800

    Add label creation

--- srcs/train.py
+++ srcs/train.py
@@ -3,11 +3,26 @@ import os
 
 import cv2
 import torch
+import torch.nn as nn
 
 from unet import UNet
 
 
-def train(aug_data_path, device):
+def create_label_data(anno_img, output_size):
+    anno_img = cv2.resize(
+        anno_img, (output_size, output_size), interpolation=cv2.INTER_NEAREST
+    )
+    label = torch.zeros((2, output_size, output_size))
+    # Check if pixel is (255, 255, 255) and update the label
+    white_mask = (anno_img == [255, 255, 255]).all(axis=-1)
+    label[1][white_mask] = 1  # Set channel 1 to 1 where mask is True
+    label[0][~white_mask] = 1  # Set channel 0 to 1 where mask is False
+    label = label.unsqueeze(0)
+
+    return label
+
+
+def train(aug_data_path, output_size, learning_rate, device):
     img_path_list = glob.glob(os.path.join(aug_data_path, "*.png"))
     img_paths = [
         path
@@ -18,12 +33,29 @@ def train(aug_data_path, device):
     img_paths.sort()
     anno_paths.sort()
     model = UNet().to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    # TODO: Think about loss
+    loss_fn = nn.MSELoss()
+
     for i in range(len(img_paths)):
         img_path = img_paths[i]
         img = cv2.imread(img_path)
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        # TODO: Fix error
-        model.forward(gray_img)
+        tensor = torch.from_numpy(gray_img).float()
+        tensor = tensor.unsqueeze(0).unsqueeze(0).to(device)  # Shape: (1, 1, 572, 572)
+        pred = model.forward(tensor)
+
+        anno_img_path = anno_paths[i]
+        anno_img = cv2.imread(anno_img_path)
+        label = create_label_data(anno_img, output_size)
+        loss = loss_fn(pred, label.to(device))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print(pred.shape)
+        print(label.shape)
         input()
 
     # print(model)
@@ -39,4 +71,5 @@ if __name__ == "__main__":
     )
     print("Using {0} device".format(device))
     aug_data_path = "../resources/augmentation"
-    train(aug_data_path, device)
+    output_size = 388
+    train(aug_data_path, output_size, device)
