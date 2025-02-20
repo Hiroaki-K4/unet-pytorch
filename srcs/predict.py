d132b601efeac65f44d4789578385d0ceb3d86f2
Hiroaki-K4 <49no19@gmail.com>
2025-02-20 10:36:14 -0800

    Fix prediction code

--- srcs/predict.py
+++ srcs/predict.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 import numpy as np
 import torch
 
@@ -9,7 +10,7 @@ def create_segmentation_image(pred):
     result = (torch.sigmoid(pred) > 0.5).int()
     result *= 255
     if result.shape[0] == 1:
-        result = result.squeeze(0)  # Now shape is (388, 388)
+        result = result.squeeze(0)
     array = result.cpu().numpy()
     array_3_channel = np.repeat(array[:, :, np.newaxis], 3, axis=2)
 
@@ -26,8 +27,8 @@ def predict(image_path, model_path, device):
     tensor = tensor.unsqueeze(0).unsqueeze(0).to(device)  # Shape: (1, 1, 572, 572)
     tensor = tensor / 255.0
     pred = model.forward(tensor)
-    seg_img = create_segmentation_image(pred[0])
-    cv2.imwrite("test.png", seg_img)
+
+    return pred
 
 
 if __name__ == "__main__":
@@ -39,6 +40,24 @@ if __name__ == "__main__":
         else "cpu"
     )
     print("Using {0} device".format(device))
-    image_path = "../resources/augmentation/00001.png"
     model_path = "unet.pth"
-    predict(image_path, model_path, device)
+    image_dir = "../resources/augmentation"
+    output_dir = "../resources/output"
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    img_num = 5
+    for i in range(img_num):
+        img_filename = f"{i:05}.png"
+        image_path = os.path.join(image_dir, img_filename)
+        pred = predict(image_path, model_path, device)
+        seg_img = create_segmentation_image(pred[0])
+        output_path = os.path.join(output_dir, img_filename)
+        input_img = cv2.imread(image_path)
+        seg_img = cv2.resize(
+            seg_img,
+            (input_img.shape[1], input_img.shape[0]),
+            interpolation=cv2.INTER_NEAREST,
+        )
+        seg_img = seg_img.astype(np.uint8)
+        blended = cv2.addWeighted(input_img, 0.7, seg_img, 0.3, 0)
+        cv2.imwrite(output_path, blended)
